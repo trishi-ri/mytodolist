8bd438104d0b355e428bf0d1e5309fe524ae9149
trishi-ri <trishi-ri@ya.ru>
2017-03-05 18:18:19 +0300

    the remaining options for the validation

--- src/app/components/form-create-article/form-create-article.component.ts
+++ src/app/components/form-create-article/form-create-article.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators, AbstractControl} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators, AbstractControl, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'form-create-article',
@@ -9,18 +9,38 @@ import {FormBuilder, FormGroup, Validators, AbstractControl} from "@angular/form
 export class FormCreateArticleComponent{
 
   myForm: FormGroup;
+  title: string;
   link: AbstractControl;
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
-      'title': [''],
-      'link': ['', Validators.required]
+      'title': ['', Validators.required],
+      'link': ['', Validators.compose([
+        Validators.required, linkValidator
+      ])]
     });
     this.link = this.myForm.controls['link'];
+
+    this.link.valueChanges.subscribe(
+      (value: string) => {
+        console.log('link changed to: ', value);
+      }
+    );
+    this.myForm.valueChanges.subscribe(
+      (form: string) => {
+        console.log('form changed to: ', form);
+      }
+    );
   }
 
   onSubmit(value: string): void {
     console.log('you submitted value: ', value);
   }
 
+}
+
+function linkValidator(control: FormControl): { [s: string]: boolean} {
+  if (!control.value.match(/^http/)){
+    return {invalidLink: true};
+  }
 }
